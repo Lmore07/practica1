adcc747484c7ce3f5e110e9deb86fe4434872af5
Lmore07 <lmoreirat@uteq.edu.ec>
2022-06-08 19:34:07 -0500

    trabajo completo

--- practica1/app/src/main/java/com/example/practica1/MainActivity.kt
+++ practica1/app/src/main/java/com/example/practica1/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.practica1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btn_enviar_click(view: View) {
+        val intent = Intent(this, MainActivity2::class.java);
+        val txtNombre = findViewById<EditText>(R.id.txt_nombre);
+        val txt_fecha = findViewById<EditText>(R.id.txt_date);
+        val rbt_masc = findViewById<RadioButton>(R.id.rbt_masc)
+        val rbt_fem = findViewById<RadioButton>(R.id.rbt_fem);
+        val txt_phone = findViewById<EditText>(R.id.txt_phone);
+
+        val b = Bundle();
+        b.putString("NOMBRE", txtNombre.getText().toString());
+        b.putString("FECHA", txt_fecha.getText().toString());
+        if(rbt_masc.isChecked){
+            b.putString("GENERO", rbt_masc.getText().toString());
+        }else{
+            b.putString("GENERO", rbt_fem.getText().toString());
+        }
+        b.putString("TELEFONO", txt_phone.getText().toString());
+        intent.putExtras(b);
+        startActivity(intent);
     }
 }
--- practica1/app/src/main/java/com/example/practica1/MainActivity2.kt
+++ practica1/app/src/main/java/com/example/practica1/MainActivity2.kt
@@ -1,11 +1,30 @@
 package com.example.practica1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val  txtNombre = findViewById<TextView>(R.id.lbl_nombre);
+        val  txt_fecha = findViewById<TextView>(R.id.lbl_fecha);
+        val  txt_phone = findViewById<TextView>(R.id.lbl_phone);
+        val  txt_gender = findViewById<TextView>(R.id.lbl_genero);
+        //Recuperamos la información pasada en el intent
+        val bundle = this.getIntent().getExtras();
+        // //Construimos el mensaje a mostrar
+        txtNombre.setText(bundle?.getString("NOMBRE"));
+        txt_fecha.setText(bundle?.getString("FECHA"));
+        txt_phone.setText(bundle?.getString("TELEFONO"));
+        txt_gender.setText(bundle?.getString("GENERO"));
+    }
+
+    fun btn_regresa_click(v: View){
+        val intent = Intent(this, MainActivity::class.java);
+        startActivity(intent);
     }
 }
